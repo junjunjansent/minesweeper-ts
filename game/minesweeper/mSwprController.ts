66dabc78eb6abcadd6b2cc71565f09c6997c5ce6
Jansen <57595871+junjunjansent@users.noreply.github.com>
2025-05-13 16:08:43 +0800

    Initial Button Difficulty

---
+++ game/minesweeper/mSwprController.ts
@@ -0,0 +1,12 @@
+const resetArray = () => {
+  return new Array(5);
+};
+
+const main = () => {
+  //   const startInput = prompt(
+  //     "What level would you like? Type Easy, Med, OR Hard"
+  //   )?.toLowerCase;
+  // const startInput = prompt("Input guess here OR type 'reset' for game to restart");
+};
+
+main();
