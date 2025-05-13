c0e6f02234edfb7288709017a42e371f9a9f4af2
Jansen <57595871+junjunjansent@users.noreply.github.com>
2025-05-13 09:54:11 +0800

    Initial Commit

---
+++ app.ts
@@ -0,0 +1,15 @@
+
+
+const resetArray = () => {
+    return new Array(5)
+}
+
+const main = () => {
+    const startInput = prompt("What level would you like? Type Easy, Med, OR Hard")?.toLowerCase;
+
+    if 
+
+    // const startInput = prompt("Input guess here OR type 'reset' for game to restart");
+}
+
+main();
